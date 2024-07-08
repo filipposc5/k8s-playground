c2582646ba7cbc5def7af32c7463d3339d8b66c2
Filippos Chalvatzoglou <filippos@gmail.com>
2024-07-09 00:25:59 +0100

    Config and add more tools

--- flux.sh
+++ flux.sh
@@ -21,5 +21,14 @@ kubectl create cm foo --from-literal a=b --dry-run=client -o yaml > cm1.yaml
 
 aws s3 --endpoint http://${minio_ip}:9000 ls s3://fluxcd/
 
+
 # gotcha: --endpoint requires no scheme, otherwise throws weird error "Endpoint url cannot have fully qualified paths."
-flux create source bucket minio  --endpoint minio.default.svc.cluster.local.:9000 --bucket-name fluxcd --insecure --secret-ref minio-bucket-secret -n flux-system --interval=2h
+flux create source bucket minio  --endpoint minio.default.svc.cluster.local.:9001 --bucket-name fluxcd --insecure --secret-ref minio-bucket-secret -n flux-system --interval=2h
+
+aws s3 --endpoint http://${minio_ip}:9000 cp cm1.yaml s3://fluxcd/
+
+wget https://github.com/derailed/k9s/releases/download/v0.32.5/k9s_Linux_amd64.tar.gz
+echo 33c31bf5feba292b59b8dabe5547cb52ab565521ee5619b52eb4bd4bf226cea3  k9s_Linux_amd64.tar.gz > sum
+sha256sum -c sum
+tar zvxf k9s_Linux_amd64.tar.gz k9s
+sudo mv k9s /usr/local/bin
